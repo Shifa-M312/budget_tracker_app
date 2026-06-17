d785038df1c7a70eb4fe94854c02c46f1a964d14
Shifa-M312 <shifanarasheedm@gmail.com>
2026-06-18 01:30:50 +0530

    fix: update frontend paths to use production server url

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ function Login() {
 
     try {
       // Direct, absolute login endpoint link
-      const { data } = await axios.post("https://onrender.com", {
+      const { data } = await axios.post("https://budget-tracker-app-skpt.onrender.com", {
         email,
         password,
       });
