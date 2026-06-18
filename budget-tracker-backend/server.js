ee9ce49e922d30f5f3cb7de6e88b5471d15034bc
Shifa-M312 <shifanarasheedm@gmail.com>
2026-06-19 01:12:49 +0530

    fixing

--- budget-tracker-backend/server.js
+++ budget-tracker-backend/server.js
@@ -18,12 +18,15 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options('*', (req, res) => {
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS')  {
   res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.sendStatus(200);
+  }
+  next();
 });
 
 
