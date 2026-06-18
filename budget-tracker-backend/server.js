ee05e2003b8ee1501315678e8a7254f7f0ee5a56
Shifa-M312 <shifanarasheedm@gmail.com>
2026-06-19 01:08:20 +0530

    fixing

--- budget-tracker-backend/server.js
+++ budget-tracker-backend/server.js
@@ -12,15 +12,19 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: ['https://budget-tracker-app-bay.vercel.app', 'http://localhost:5173', 'http://localhost:3000'],
+  origin: true,
+
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options('*', cors({
-  origin: ['https://budget-tracker-app-bay.vercel.app', 'http://localhost:5173', 'http://localhost:3000'],
-  credentials: true
-}));
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.sendStatus(200);
+});
 
 
 
